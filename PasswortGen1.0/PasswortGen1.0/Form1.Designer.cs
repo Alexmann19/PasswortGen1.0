5f7fce297c0ee1c82b10bd39d7d64000e832ccc9
Alexander Lehr <Alexlehr@hotmail.de>
2019-04-01 10:52:04 +0200

    add programm

---
+++ PasswortGen1.0/PasswortGen1.0/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace PasswortGen1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbpasswort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbanz = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbspeichern = new System.Windows.Forms.TextBox();
+            this.tbladen = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbpasswort
+            // 
+            this.tbpasswort.Location = new System.Drawing.Point(71, 5);
+            this.tbpasswort.Name = "tbpasswort";
+            this.tbpasswort.ReadOnly = true;
+            this.tbpasswort.Size = new System.Drawing.Size(280, 20);
+            this.tbpasswort.TabIndex = 0;
+            this.tbpasswort.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Passwort:";
+            // 
+            // tbanz
+            // 
+            this.tbanz.Location = new System.Drawing.Point(120, 31);
+            this.tbanz.MaxLength = 2;
+            this.tbanz.Name = "tbanz";
+            this.tbanz.Size = new System.Drawing.Size(31, 20);
+            this.tbanz.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Anzahl der Zeichen:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Passwort generieren";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Speichern";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 51);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Laden";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbladen);
+            this.groupBox1.Controls.Add(this.tbspeichern);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 118);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(339, 87);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Speichern/Laden";
+            // 
+            // tbspeichern
+            // 
+            this.tbspeichern.Location = new System.Drawing.Point(177, 24);
+            this.tbspeichern.Name = "tbspeichern";
+            this.tbspeichern.Size = new System.Drawing.Size(156, 20);
+            this.tbspeichern.TabIndex = 7;
+            // 
+            // tbladen
+            // 
+            this.tbladen.Location = new System.Drawing.Point(177, 50);
+            this.tbladen.Name = "tbladen";
+            this.tbladen.Size = new System.Drawing.Size(156, 20);
+            this.tbladen.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Speichername:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(87, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Name der Datei:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 213);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbanz);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbpasswort);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PasswortGen1.0";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbpasswort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbanz;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbladen;
+        private System.Windows.Forms.TextBox tbspeichern;
+    }
+}
+
---
+++ PasswortGen1.0/PasswortGen1.0/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PasswortGen1._0
+{
+    public partial class Form1 : Form
+    {
+        public static String pfad1 = @"C:\PasswortGen\";
+        public static String pfad2 = ".ini";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool anza = true;
+            int anz = 0;
+            string anzs = tbanz.Text;
+            if (tbanz.Text == "")
+            {
+                MessageBox.Show("Sie haben keine Zahl angegeben!", "Fehler", MessageBoxButtons.OK);
+            }
+            else
+            foreach (char x in anzs)
+            {
+                if (!Char.IsDigit(x))
+                {
+                    anza = false;
+                }
+                if (anza == false)
+                {
+                    MessageBox.Show("Sie dürfen nur Zahlen eingeben!", "Fehler", MessageBoxButtons.OK);
+                }
+                else
+                if (anzs.Length == 2)
+                {
+                    anza = true;
+                    if (!Char.IsDigit(anzs[1]))
+                    {
+                        anza = false;
+                    }
+                    if (anza == false)
+                    {
+                            MessageBox.Show("Sie dürfen nur Zahlen eingeben!", "Fehler", MessageBoxButtons.OK);
+                    }
+                    anz = Convert.ToInt32(anzs);
+                }
+                else
+                anz = Convert.ToInt32(anzs);
+
+            }
+            
+
+            string ret = string.Empty;
+            System.Text.StringBuilder SB = new System.Text.StringBuilder();
+            string Content = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw!öäüÖÄÜß";
+            Random rnd = new Random();
+            for (int i = 0; i < anz; i++)
+            {
+                SB.Append(Content[rnd.Next(Content.Length)]);
+            }
+            string passwort = SB.ToString();
+            tbpasswort.Text = passwort;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tbpasswort.Text == String.Empty)
+            {
+                MessageBox.Show("Es wurde kein Passwort genneriert", "Fehler", MessageBoxButtons.OK);
+            }
+            else
+            if (tbspeichern.Text == String.Empty)
+            {
+                MessageBox.Show("Sie haben keinen Dateinamen angegeben", "Fehler", MessageBoxButtons.OK);
+            }
+            else
+            if (!Directory.Exists(pfad1))
+            {
+                System.IO.Directory.CreateDirectory(pfad1);
+            }
+            else
+            if (File.Exists(pfad1 + tbspeichern.Text + pfad2))
+            {
+                File.Delete(pfad1 + tbspeichern.Text + pfad2);
+            }
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(pfad1 + tbspeichern.Text + pfad2, true))
+            {
+                file.WriteLine(tbpasswort.Text);
+            }
+            if (tbspeichern.Text != String.Empty)
+            {
+                MessageBox.Show("Sie haben die Zahlen erfoldreich gespeichert", "Erfolg", MessageBoxButtons.OK);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] eingabe = null;
+            try
+            {
+                // einlesen der Datei in ein String-Array
+                eingabe = File.ReadAllLines(pfad1 + tbladen.Text + pfad2, Encoding.UTF8);
+            }
+            catch (Exception ex)
+            {
+                // Datei nicht vorhanden ...
+                MessageBox.Show("Fehler beim Einlesen:\r\n", ex.Message);
+                return;
+            }
+            tbpasswort.Text = eingabe[0];
+            MessageBox.Show("Sie haben das Passwort erfolgreich geladen", "Laden erfolgreich", MessageBoxButtons.OK);
+        }
+    }
+}
